649de69e88f31126aeee4a98676abd67b2b4f96f
rarDevelopment <rar@dev>
2022-09-04 13:27:00 -0400

    finished tutorial, included rar sprite

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,42 @@
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
 local gfx <const> = playdate.graphics;
 
+local playerSprite = nil
+local playerSpeed = 4
+local playTimer = nil
+local playTime = 30 * 1000
+local coinSprite = nil
+local score = 0
+
+local function resetTimer()
+	playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
+end
+
+local function moveCoin()
+	local randX = math.random(40, 360)
+	local randY = math.random(40, 200)
+	coinSprite:moveTo(randX, randY)
+end
+
+
 local function initalize()
+	math.randomseed(playdate.getSecondsSinceEpoch())
+
     local playerImage = gfx.image.new("images/player")
+	playerSprite = gfx.sprite.new(playerImage);
+	playerSprite:moveTo(200, 120)
+	playerSprite:setCollideRect(0,0,playerSprite:getSize())
+	playerSprite:add(); -- adds to the draw list
+
+	local coinImage = gfx.image.new("images/coin")
+	coinSprite = gfx.sprite.new(coinImage)
+	moveCoin()
+	coinSprite:setCollideRect(0,0,coinSprite:getSize())
+	coinSprite:add(); -- adds to the draw list
 
 	local backgroundImage = gfx.image.new("images/background")
 	gfx.sprite.setBackgroundDrawingCallback(
@@ -15,10 +46,40 @@ local function initalize()
 			gfx.clearClipRect()
 		end
 	)
+	resetTimer()
 end
 
 initalize()
 
 function playdate.update()
+	if playTimer.value == 0 then
+		if playdate.buttonJustPressed(playdate.kButtonA) then
+			resetTimer()
+			moveCoin()
+		end
+	end
+	if playdate.buttonIsPressed(playdate.kButtonUp) then
+		playerSprite:moveBy(0, -playerSpeed)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonDown) then
+		playerSprite:moveBy(0, playerSpeed)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonLeft) then
+		playerSprite:moveBy(-playerSpeed, 0)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonRight) then
+		playerSprite:moveBy(playerSpeed, 0)
+	end
+
+	local collisions = coinSprite:overlappingSprites()
+	if #collisions >= 1 then
+		moveCoin()
+		score = score + 1
+	end
+
+	playdate.timer.updateTimers();
 	gfx.sprite.update();
+
+	gfx.drawText("Time: " .. math.ceil(playTimer.value/1000),5,5)
+	gfx.drawText("Score: " .. score, 320, 5)
 end
